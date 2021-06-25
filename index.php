21b0f39e8795c72c90849ffbd8b072d0b58ac451
philippe-blanquier <philippe.blanquier@gmail.com>
2021-06-25 16:13:34 +0200

    modif PhB 2

--- index.php
+++ index.php
@@ -1,28 +1,28 @@
 <?php
+define('XML_SOURCE_FILE','source.xml');
+
 // Le fichier source DOIT être accessible
-if (file_exists('source.xml')) 
+if (file_exists(XML_SOURCE_FILE)) 
 {
-    // Il est !
-    $xml= simplexml_load_file('source.xml');
     // Si page est présent c'est que l'utilisateur à cliquer sur une option
     if (isset($_GET['page']))
     {
-        $pageIdx = intval(htmlspecialchars($_GET['page']));
-        $xml= simplexml_load_file('source.xml');
-        $count= count($xml->page);
-        if ($pageIdx >= $count)
-        {
-            echo 'Numéro de page inconnu';
-            exit();        
-        }
+        $pageNumber= intval(htmlspecialchars($_GET['page']));
     } 
     else 
     {
         // C'est la première fois que le script est lancé. On prend la page d'acceuil par défaut
-        $pageIdx= 0;
+        $pageNumber= 0;
+    }
+    $xmlFile= simplexml_load_file(XML_SOURCE_FILE);
+    $pageNumberMax= count($xmlFile->page);
+    if ($pageNumber >= $pageNumberMax)
+    {
+        echo 'Numéro de page inconnu';
+        exit();        
     }
     // préparons le titre de la page qui sera affiché
-    $title= $xml->page[$pageIdx]->title;
+    $title= $xmlFile->page[$pageNumber]->title;
 }
 else
 {
@@ -35,8 +35,7 @@ else
 <html lang="fr">
     <head>
         <meta charset="utf-8" />
-         <link rel="stylesheet" href="/assets/css/style.css" />
-        <title><?= $title; ?></title>
+        <title><?= $title ?></title>
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -44,17 +43,17 @@ else
                 <span class="navbar-toggler-icon"></span>
             </button>
             <?php
-                for ($i = 0; $i != $count; $i++) 
+                for ($i = 0; $i < $pageNumberMax; $i++) 
                 {
-                    $menu = $xml->page[$i]->menu;
-                    $display= '<a class="navbar-brand ml-5" href="index.php?page='.$i.'">'.$menu.'</a>';
+                    $menu = $xmlFile->page[$i]->menu;
+                    $display= '<a class="nav-link ml-5" href="index.php?page='.$i.'">'.$menu.'</a>';
                     echo $display;
                 }
             ?>        
         </nav>
         <!-- Affichage du contenu correspondant à la page souhaitée -->
         <div class="container content-page">
-            <?= $xml->page[$pageIdx]->content; ?>
+            <?= $xmlFile->page[$pageNumber]->content; ?>
         </div>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
