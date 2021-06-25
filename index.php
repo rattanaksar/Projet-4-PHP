57344b461ec649fcc7ad673fec1210bd5b5e9ba2
rattanaksar <81554110+rattanaksar@users.noreply.github.com>
2021-06-25 16:17:44 +0200

    Modif PHP

--- index.php
+++ index.php
@@ -35,16 +35,19 @@ else
 <html lang="fr">
     <head>
         <meta charset="utf-8" />
-         <link rel="stylesheet" href="/assets/css/style.css" />
+        <link rel="stylesheet" href="/assets/css/style.css" />
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <title><?= $title; ?></title>
     </head>
     <body>
+
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <?php
-                for ($i = 0; $i != $count; $i++) 
+                for ($i = 0; $i != $count; $i++)
                 {
                     $menu = $xml->page[$i]->menu;
                     $display= '<a class="navbar-brand ml-5" href="index.php?page='.$i.'">'.$menu.'</a>';
@@ -55,9 +58,5 @@ else
         <!-- Affichage du contenu correspondant à la page souhaitée -->
         <div class="container content-page">
             <?= $xml->page[$pageIdx]->content; ?>
-        </div>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    </div>
 </body>
 </html>
