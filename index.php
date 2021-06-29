7df252ad3408e049803a7297b7c712a6cccaf953
Gautier Yrondy <gautieryrondy@gmail.com>
2021-06-29 09:56:11 +0200

    Mise à jour de l'index et du .htaccess pour concorder avec la version IB commit 51

--- index.php
+++ index.php
@@ -1,21 +1,29 @@
 <?php
-
-if (file_exists('source.xml')) {
+if (file_exists('source.xml')) 
+{
     $xml = simplexml_load_file('source.xml'); //simplexml_load_file — Convertit un fichier XML en objet
-    if (isset($_GET['page'])) {
-        $pageNumber = intval(htmlspecialchars($_GET['page'])); //intval — Retourne la valeur numérique entière équivalente d'une variable
-    } else {
-        $pageNumber = 0;
+    $pageNumberMax= count($xml->page); // La fonction count() compte les enfants d'un nœud spécifié.
+    $pageNumber= 0;
+    if (isset($_GET['id'])) 
+    {
+        $idNumber = htmlspecialchars($_GET['id']);
+        for ($i = 0; $i < $pageNumberMax; $i++)
+        {
+            if ($xml->page[$i]['id'] == $idNumber)
+            {
+                $pageNumber= $i;
+                break;
+            }
+        }
+        $title = $xml->page[$pageNumber]->title;
     }
     $pageNumberMax = count($xml->page); // La fonction count() compte les enfants d'un nœud spécifié.
     if ($pageNumber >= $pageNumberMax) {
         echo 'Numéro de page inconnu';
-        exit();
     }
-    $title = $xml->page[0]->title;
+    $title = $xml->page[$pageNumber]->title;
 } else {
     echo 'Pas de fichier source.xml';
-    exit();
 }
 ?>
 
@@ -26,7 +34,7 @@ if (file_exists('source.xml')) {
     <meta charset="utf-8" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link href="style.css" rel="stylesheet">
-    <title><?= $title; ?></title>
+    <title><?= $title ?></title>
 </head>
 
 <body>
@@ -35,23 +43,22 @@ if (file_exists('source.xml')) {
         <a class="navbar-brand ms-5" href="index.php?page=0">
             <img src="assets/img/logo.png" alt="logo Ocordo" width="100" height="100">
         </a>
-
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <!-- on génère les différentes pages du site -->
         <?php
         for ($i = 0; $i < $pageNumberMax; $i++) {
-            $menu = $xml->page[$i]->menu;
-            $displayPages = '<div class="collapse navbar-collapse" id="navbarToggler"><a class="nav-link text-secondary ms-5" href="index.php?page=' . $i . '">' . $menu . '</a></div>';
-            echo $displayPages;
-        }
-        ?>
+            $menu = $xml->page[$i]->menu; ?>
+            <div class="collapse navbar-collapse" id="navbarToggler">
+                <a class="nav-link text-secondary ms-5" href="<?= $xml->page[$i]['id'] ?>.html"><?= $menu ?></a>
+            </div>
+        <?php
+        } ?>
     </nav>
-    <div class="container content-page">
+    <div class=" container content-page">
         <?= $xml->page[$pageNumber]->content; ?>
     </div>
-    <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </body>
 
